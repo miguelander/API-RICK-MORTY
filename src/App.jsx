1164715fc5602b7aad142336f7a9b93c5dc33b41
miguelonofre <onofremiguel235@gmail.com>
2025-09-12 17:28:37 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Tailwind from './assets/tailwind'
+
+function App() {
+  
+
+  return (
+    <div>
+      
+    <Tailwind/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/CharactersApi.jsx
@@ -0,0 +1,8 @@
+
+
+const CharactersApi = () => {
+
+   
+}
+
+export default CharactersApi
